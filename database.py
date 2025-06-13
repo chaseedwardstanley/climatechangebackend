cf0289b0b9450185f1c050eafa0b6c70e6ca3833
cestanle <cestanle@purdue.edu>
2025-06-13 19:35:43 -0400

    update

--- database.py
+++ database.py
@@ -8,8 +8,24 @@ class ServerDatabase:
         self.cur = self.conn.cursor()
 
     def get_all_objects(self):
-        self.cur.execute("SELECT * FROM world_objects;")
-        return self.cur.fetchall()
+        try:
+            with self.conn.cursor() as cur:
+                cur.execute("SELECT id, type, position_x, position_y, state FROM world_objects;")
+                rows = cur.fetchall()
+                result = []
+                for row in rows:
+                    result.append({
+                        "id": row[0],
+                        "type": row[1],
+                        "position_x": row[2],
+                        "position_y": row[3],
+                        "state": row[4]  # This is already JSON
+                    })
+                return result
+        except Exception as e:
+            print("DB ERROR:", e)
+            self.conn.rollback()
+            return []
 
     def update_object_state(self, object_id, new_state):
         self.cur.execute("UPDATE world_objects SET state = %s WHERE id = %s;", (new_state, object_id))
